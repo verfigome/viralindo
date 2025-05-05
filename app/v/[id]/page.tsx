2557028c0295302fb2a8782584c91622f75d7466
verfigome <verfigome@gmail.com>
2025-05-05 10:40:56 +0700

    Update page.tsx

--- app/v/[id]/page.tsx
+++ app/v/[id]/page.tsx
@@ -160,7 +160,7 @@ const jsonLd = {
         {/* ... */}
         </section>
             <iframe
-                className="w-full h-[30vh] md:h-[55vh] lg:h-[70vh]"
+                className="w-full h-[55vh] md:h-[55vh] lg:h-[70vh]"
                 src={`https://d000d.com/e/${file.filecode}`}
                 scrolling="no"
                 title={file.title}
